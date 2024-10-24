04e088f688cb6b8d5ec310d8cc63c06e3c8c477b
Nguyen Vo Anh Minh <minhnguyen.31201021885@st.ueh.edu.vn>
2024-10-24 06:50:40 +0000

    feat: add function definition

--- number_guess.sh
+++ number_guess.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+# define a function for guessing game
+GUESS_GAME() {
+  # random number between 1 and 1000
+  RANDOM=123456
+  SECRET_NUMBER=$(( $RANDOM % 1001 ))
+  # insert new game
+  NEW_GAME=$($PSQL "INSERT INTO games(user_id, secret_number) VALUES($USER_ID, $SECRET_NUMBER)")
+  NEW_GAME_ID=$($PSQL "SELECT MAX(game_id) FROM games")
+  # declare guess variable
+  NUM_GUESS=""
+  # declare tries variable
+  TRIES=0
+  # request input
+  echo "Guess the secret number between 1 and 1000:"
+}
